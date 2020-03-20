b90d5689436b802acb90466538b0779ec47f871f
jkbicierro <61955888+jkbicierro@users.noreply.github.com>
2020-03-20 20:45:14 +0800

    w

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
 const { Client, MessageEmbed } = require('discord.js');
 
-
+const eco = require("discord-economy");
 const client = new Client();
-const rainbow = require("./rainbow.json")
 
 const settings = {
     prefix: '/',
     token: 'Njg2MDg2MDU4Njc2NDUzMzg1.XnVaww.tevCsvZZ1e26s6xcXNbSnKnRby4',
+    confess: '😈｜anonymous',
+    general: '🌸｜general-chat',
     svr: 'Lowkid 낮은아이 PH'
 }
 
@@ -16,16 +17,14 @@ client.on('ready', () => {
 });
 
 client.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.cache.find(channel => channel.name === "general")
+    const channel = member.guild.channels.cache.find(channel => channel.name === settings.general)
     if(!channel) return;
-    //channel.send(`> Welcome to **Lowkid**, *${member}, please read the rules.*`);
 
     const embed = new MessageEmbed()
     .setTitle(settings.svr)
-    .setThumbnail(message.author.displayAvatarURL())
+    .setThumbnail('https://i.imgur.com/w0y9l7X.png')
     .setAuthor('Lowkid', 'https://i.imgur.com/w0y9l7X.png', 'https://discord.gg/n5PGyV')
-    .addField('Username:', message.member.user.tag, true)
-    .addField('Member ID:', message.member.id, true)
+    .addField('Username:', `${member}`, true)
     .setColor('RANDOM')
     .setFooter('Copyright LWKD 2020', 'https://i.imgur.com/w0y9l7X.png')
     .setDescription('Welcome to Lowkid this server is for people to socialize, and interests such as anime, manga, games, art, and more to be shared as one!');
@@ -39,8 +38,32 @@ client.on('message', async message =>
     
     if (!message.content.startsWith(settings.prefix) || message.author.bot) return;
 
+    // Economy
+    if (command === 'balance') {
+ 
+        var output = await eco.FetchBalance(message.author.id)
+        message.channel.send(`Hey ${message.author.tag}! You own ${output.balance} lowbucks.`);
+    }
+    if (command === 'daily') {
+ 
+        var output = await eco.Daily(message.author.id)
+        //output.updated will tell you if the user already claimed his/her daily yes or no.
+     
+        if (output.updated) {
+     
+          var profile = await eco.AddToBalance(message.author.id, 5)
+          message.reply(`You claimed your daily coins successfully! You now own ${profile.newbalance} lowbucks.`);
+     
+        } else {
+          message.channel.send(`Sorry, you already claimed your daily lowbucks!\nBut no worries, over ${output.timetowait} you can daily again!`)
+        }
+    }
+    if (command === 'lowbucks') {
+        message.channel.send(`under maintenance`)
+    }
+    // Confess
     if (command === 'confess') {
-        const confess = message.guild.channels.cache.find(confess => confess.name === "confession")
+        const confess = message.guild.channels.cache.find(confess => confess.name === settings.confess)
         if(!confess) return;
         mentionMessage = message.content.slice (8);
         const embed = new MessageEmbed()
@@ -52,8 +75,7 @@ client.on('message', async message =>
         message.delete();
         confess.send(embed);
     }
-
-    if (command === 'welcome') {
+    /*if (command === 'welcome') {
         const embed = new MessageEmbed()
         .setTitle(settings.svr)
         .setThumbnail(message.author.displayAvatarURL())
@@ -64,7 +86,7 @@ client.on('message', async message =>
         .setFooter('Copyright LWKD 2020', 'https://i.imgur.com/w0y9l7X.png')
         .setDescription('Welcome to Lowkid this server is for people to socialize, and interests such as anime, manga, games, art, and more to be shared as one!');
         message.channel.send(embed);
-    }
+    }*/
     if (command === 'avatar') {
         var user;
         user = message.mentions.users.first(); 
@@ -84,26 +106,6 @@ client.on('message', async message =>
         else { 
             GetUserAvatar(user);
         }
-    }
-    if (command === 'rainbow') {
-        const rolez = message.mentions.roles.first() || message.guild.roles.find(r=> r.name === args [0])
-        if(!rolez) return message.channel.send(rainbow.messageresponse.rolenotfound).catch(err=> message.channel.send("No response"))
-        if(!message.guild.member(bot.user.id).hasPermission("MANAGE_ROLES")) return message.channel.send(rainbow.messageresponse.missingperm).catch(err=> message.channel.send("no response"))
-        var colors = rainbow.rainbowrole
-        var rolestart = setInterval(function() {
-            var colorsz = colors[Math.floor(Math.random() * colors.length)];
-            rolez.setColor(colorsz)
-        }, rainbow.rainbowdelay); 
-            message.channel.send(rainbow.messageresponse.success).catch(err=> message.channel.send("No response"))
-
-    }
-    if (command === 'stoprainbow')
-    {
-        setTimeout(function () 
-        {
-            process.exit()
-        }, 1000);
-        message.channel.send(rainbow.messageresponse.rainbowstop).catch(err=> message.channel.send("No response"))
     }
     function GetUserAvatar(user) {
         const embed = new MessageEmbed()

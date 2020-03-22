480b8ef9c8d7444cd17c9138f776dc5dd3b8a9ac
jkbicierro <61955888+jkbicierro@users.noreply.github.com>
2020-03-22 09:24:55 +0800

    w

--- index.js
+++ index.js
@@ -67,6 +67,25 @@ bot.on('message', async message =>
         message.delete();
         message.channel.send(embed);
     }
+
+    if (command === 'rainbow') {
+        const rolez = message.mentions.roles.first() || message.guild.roles.find(r=> r.name === args [0])
+        if(!rolez) return message.channel.send(settings.messageresponse.rolenotfound).catch(err=> message.channel.send("No response"))
+        if(!message.guild.member(bot.user.id).hasPermission("MANAGE_ROLES")) return message.channel.send(settings.messageresponse.missingperm).catch(err=> message.channel.send("no response"))
+        var colors = settings.rainbowrole
+        var rolestart = setInterval(function() {
+            var colorsz = colors[Math.floor(Math.random() * colors.length)];
+            rolez.setColor(colorsz)
+        }, settings.rainbowdelay); 
+            message.channel.send(settings.messageresponse.success).catch(err=> message.channel.send("No response"))
+
+    }
+    if (command === 'rainbowstop') {
+            setTimeout(function () {
+           process.exit()
+            }, 1000);
+                       message.channel.send(settings.messageresponse.rainbowstop).catch(err=> message.channel.send("No response"))
+                       }
     
     if (command === 'avatar') {
         var user;

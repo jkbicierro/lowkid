6e767ee0d34958d00c2ff506bcb68ad111fe39b6
jkbicierro <61955888+jkbicierro@users.noreply.github.com>
2020-03-22 11:29:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ bot.on('message', async message =>
 
     // Confess
     if (command === 'confess') {
-        const confess = message.guild.channels.get("682110743503437845")
+        const confess = message.channel.id("682110743503437845")
         if(!confess) return;
         mentionMessage = message.content.slice (8);
         const embed = new MessageEmbed()

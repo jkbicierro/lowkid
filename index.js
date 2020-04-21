9062f579609d420c41352c1f9788ad210bd82934
jkbicierro <61955888+jkbicierro@users.noreply.github.com>
2020-04-21 15:40:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,108 +1,96 @@
-
 const { Client, MessageEmbed } = require('discord.js');
-//const eco = require("discord-economy");
+const eco = require("discord-economy");
 
 const bot = new Client();
 
 const settings = {
     prefix: '/',
-    token: 'Njg2MDg2MDU4Njc2NDUzMzg1.Xovy_g.8_lIce-X5oGaZ-IZZWOf1e5Ww98',
-    general: '682110743503437845',
-    announce: '691109820903718993',
-    regans: '683531557482397712',
+    token: 'Njg2MDg2MDU4Njc2NDUzMzg1.Xp6jAA.vFAL-SbVa3nVwc6jKjf0XYUhYQU', 
+    general: '682110743503437845', // Channel ID
+    announce: '691109820903718993', // Channel ID
+    copyright: '© ᴸᵂᴷᴰ 2020',
+    svrclr: '#f08080',
     svr: 'Lowkid 낮 PH'
 }
 
-bot.on('ready', () => {
+bot.on('ready', async message => {
     console.log('Pakantot.');
-    bot.user.setActivity('ʟᴏᴡᴋɪᴅ v0.1.2');
+    bot.user.setActivity('Lowkid v0.1.2');
 });
 
-bot.on('guildMemberAdd', member => {
+bot.on('guildMemberAdd', member => { // user
     const channel = member.guild.channels.cache.find(channel => channel.id === settings.general)
     if(!channel) return;
-
     const embed = new MessageEmbed()
-    .setTitle(settings.svr)
-    .setThumbnail('https://i.imgur.com/w0y9l7X.png')
-    .addField('Username:', `${member}`, true)
-    .setColor('RANDOM')
-    .setFooter('Copyright LWKD 2020', 'https://i.imgur.com/w0y9l7X.png')
-    .setDescription('Welcome to Lowkid, this server is for people to socialize, and interests such as anime, manga, games, art, and more to be shared as one!');
-    channel.send(embed);
+    .setAuthor(`${member.user.tag}`, member.user.displayAvatarURL())
+    .setThumbnail(member.user.displayAvatarURL())
+    .setColor(settings.svrclr)
+    //.setFooter(settings.copyright, 'https://i.imgur.com/w0y9l7X.png')
+    .setDescription(`Welcome to **Lowkid**, ${member}. *this server is for people to socialize, and interests such as **anime, manga, games, art, and more** to be shared as one!*`);
+    //channel.send(embed);
+    const emoji = member.guild.emojis.cache.find(emoji => emoji.name === 'welcome');
+    const emoji1 = member.guild.emojis.cache.find(emoji => emoji.name === 'welcome1');
+    channel.send({embed: embed}).then(embedMessage => {
+        embedMessage.react(emoji);
+        embedMessage.react(emoji1);
+    });
+
 });
 
-bot.on('message', async message => 
+bot.on('message', async message =>  //author
 {
     var command = message.content.toLowerCase().slice(settings.prefix.length).split(' ')[0];
     var args = message.content.split(' ').slice(1);
     
     if (!message.content.startsWith(settings.prefix) || message.author.bot) return;
 
-    /*if (command === 'balance') {
-        var output = await eco.FetchBalance(message.author.id)
-        //const confess = message.guild.channels.cache.find(confess => confess.id === settings.announce)
-        //if(!confess) return;
-        const embed = new MessageEmbed()
-        .setTitle(`${message.author.tag}`)
-        .setColor('RANDOM')
-        .addField('Balance', `${output.balance}`, true)
-        .setThumbnail('https://i.imgur.com/w0y9l7X.png')
-        .setFooter('Copyright LWKD 2020', 'https://i.imgur.com/w0y9l7X.png');
-        message.channel.send(embed);
-    }
     if (command === 'bal') {
         var output = await eco.FetchBalance(message.author.id)
-        message.channel.send(`Hey ${message.author.tag}! You own ${output.balance} lowbucks.`);
+        const embed = new MessageEmbed()
+        .setColor(settings.svrclr)
+        .setDescription(`💶 **${message.author.username}** currently has **${output.balance}** Lowbucks!`)
+        //.setFooter(settings.copyright, 'https://i.imgur.com/w0y9l7X.png');
+        message.channel.send(embed);
     }
     if (command === 'daily') {
-    
         var output = await eco.Daily(message.author.id)
-    
         if (output.updated) {
-    
-        var profile = await eco.AddToBalance(message.author.id, 2) //lb
-        message.reply(`You claimed your daily lowbucks successfully! You now own ${profile.newbalance} lowbucks.`);
-    
+        var profile = await eco.AddToBalance(message.author.id, 200) //lb
+        const embed = new MessageEmbed()
+        .setColor(settings.svrclr)
+        .setDescription(`💶 You claimed your daily lowbucks successfully! You now own **${profile.newbalance}** lowbucks!`)
+        //.setFooter(settings.copyright, 'https://i.imgur.com/w0y9l7X.png');
+        message.channel.send(embed);
         } else {
-        message.channel.send(`Sorry, you already claimed your daily lowbucks!\nBut no worries, over ${output.timetowait} you can daily again!`)
+        const embed = new MessageEmbed()
+        .setColor(settings.svrclr)
+        .setDescription(`💶 Sorry, you already claimed your daily lowbucks! But no worries, over **${output.timetowait}** you can daily again!`)
+        //.setFooter(settings.copyright, 'https://i.imgur.com/w0y9l7X.png');
+        message.channel.send(embed);
         }
-    }*/
-    if (command === 'fg') {
-        const confess = message.guild.channels.cache.find(confess => confess.id === settings.announce)
-        if(!confess) return;
-        awewe = message.content.slice (3);
-        const embed = new MessageEmbed()
-        .setTitle(awewe)
-        .setColor('RANDOM')
-        .setDescription('React anything to enter giveaway')
-        .setFooter('Copyright LWKD 2020', 'https://i.imgur.com/w0y9l7X.png');
-        confess.send(embed);
     }
-    if (command === 'fgf') {
-        const confess = message.guild.channels.cache.find(confess => confess.id === settings.announce)
-        if(!confess) return;
-        awwe = message.content.slice (4);
-        const embed = new MessageEmbed()
-        .setTitle('Giveaway Ended')
-        .setColor('RANDOM')
-        .addField('Lowkid Giveaway Winner:', awwe, true)
-        .setFooter('Copyright LWKD 2020', 'https://i.imgur.com/w0y9l7X.png');
-        confess.send(embed);
-    }
+
     // Confess
     if (command === 'confess') {
-        const confess = message.guild.channels.cache.find(confess => confess.id === settings.general)
-        if(!confess) return;
-        mentionMessage = message.content.slice (8);
+        params = message.content.slice (8);
+        
+        if(!params || params.length === 0) {
+            const usageembed = new MessageEmbed()
+            .setColor(settings.svrclr)
+            .setDescription(`***Usage:** /confess [text]*`);
+            
+            //.setFooter(settings.copyright, 'https://i.imgur.com/w0y9l7X.png');
+            message.reply(usageembed);
+            message.react("❌")
+            return;
+        }
         const embed = new MessageEmbed()
-        .setTitle(settings.svr)
-        .setColor('RANDOM')
-        .addField('Anonymous', mentionMessage, true)
-        .setThumbnail('https://i.imgur.com/w0y9l7X.png')
-        .setFooter('Usage: /confess [text].', 'https://i.imgur.com/w0y9l7X.png');
-        message.delete();
+        .setColor(settings.svrclr)
+        .setDescription(`**Anonymous**\n${params}`)
+        //.setFooter(settings.copyright, 'https://i.imgur.com/w0y9l7X.png');
+        const confess = bot.channels.cache.find(confess => confess.id === '701441730112651274')
         confess.send(embed);
+        message.react("👍")
     }
     if (command === 'ajoin') 
     {
@@ -112,55 +100,74 @@ bot.on('message', async message =>
             message.reply('You need to join a voice channel first!');
         }
     }
-    if (command === 'cnn') {
-        const confess = message.guild.channels.cache.find(confess => confess.id === settings.announce)
+    if (command === 'ann') {
+        const confess = bot.channels.cache.find(confess => confess.id === settings.announce)
         if(!confess) return;
         awit = message.content.slice (4);
         const embed = new MessageEmbed()
-        .setTitle(settings.svr)
-        .setColor('RANDOM')
-        .addField('Announcement', awit, true)
-        .setThumbnail('https://i.imgur.com/w0y9l7X.png')
-        .setFooter('Copyright LWKD 2020', 'https://i.imgur.com/w0y9l7X.png');
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
+        .setDescription(awit)
+        .setColor(settings.svrclr)
+        //.setFooter(settings.copyright, 'https://i.imgur.com/w0y9l7X.png');
         confess.send(embed);
     }
-    if (command === 'register') {
-        const confess = message.guild.channels.cache.find(confess => confess.id === settings.regans)
-        if(!confess) return;
-        awit = message.content.slice (9);
+
+    if (command === 'info') {
+
         const embed = new MessageEmbed()
-        .setTitle(settings.svr)
-        .setColor('RANDOM')
-        .setDescription('Registration')
-        .addField('Event:', awit, true)
-        .setThumbnail(message.author.displayAvatarURL())
-        .setFooter('Copyright LWKD 2020', 'https://i.imgur.com/w0y9l7X.png');
-        message.delete();
-        confess.send(embed);
+        .setColor(settings.svrclr)
+        .setDescription('**Discord:** *https://www.discord.io/lowkid/*\n**Website:** *https://www.ezire.ph/*\n**Facebook:** *https://www.facebook.com/lowkidapprel/*\n**Instagram:** *https://www.instagram.com/Lowkidapparel/*')
+        message.channel.send(embed);
     }
-    if (command === 'answer') {
-        const confess = message.guild.channels.cache.find(confess => confess.id === settings.regans)
-        if(!confess) return;
-        awitize = message.content.slice (7);
+
+    if (command === 'sponsor') {
+
         const embed = new MessageEmbed()
-        .setTitle(settings.svr)
-        .setColor('RANDOM')
-        .setDescription('Question and Answer')
-        .addField('Answer:', awitize, true)
-        .setThumbnail(message.author.displayAvatarURL())
-        .setFooter('Copyright LWKD 2020', 'https://i.imgur.com/w0y9l7X.png');
-        message.delete();
-        confess.send(embed);
+        .setTitle('Sponsorship Rewards')
+        .setDescription('These are our sponsorship rewards if you sponsor the following you will get the reward right next to it.')
+        .setColor(settings.svrclr)
+        .addField('💰 50 Pesos Load', 'Custom role', false)
+        .addField('💰 50 Pesos Load', 'Custom color', false)
+        .addField('💰 50 Pesos Load', 'Custom emoji', false)
+        .addField('💰 50 Pesos Load', 'Nickname access', false)
+        .addField('💰 50 Pesos Load', '+$100,000', false)
+        .addField('💰 100 Pesos Load', '+$200,000', false)
+        .addField('💰 250 Pesos Load', '+$350,000', false)
+        .addField('💰 500 Pesos Load', '+$600,000', false)
+        .addField('💰 Nitro Classic', '+$350,000', false)
+        .addField('💰 Discord Nitro', '+$600,000', false)
+
+
+        message.channel.send(embed);
     }
-    if (command === 'amsg') 
+    if (command === 'suggest') 
     {
-        mama = message.content.slice (5);
+        suggest = message.content.slice (8);
         const embed = new MessageEmbed()
-        .setTitle(settings.svr)
+        .setAuthor(`Suggested by ${message.author.tag}`, message.author.displayAvatarURL())
+        .setDescription(suggest)
+        .setColor(settings.svrclr)
+        //.setFooter(settings.copyright, 'https://i.imgur.com/w0y9l7X.png');
+        message.react("👍")
+        const emoji = message.guild.emojis.cache.find(emoji => emoji.name === '6093_Animated_Checkmark');
+        const emoji1 = message.guild.emojis.cache.find(emoji => emoji.name === 'ekis');
+        //const emoji2 = message.guild.emojis.cache.find(emoji => emoji.name === 'lowkid2');
+	    //message.react(emoji);
+        message.channel.send({embed: embed}).then(embedMessage => {
+            //embedMessage.react(emoji2);
+            embedMessage.react(emoji);
+            embedMessage.react(emoji1);
+            embedMessage.react("📪");
+        });
+    }
+    
+    if (command === 'embed') 
+    {
+        mama = message.content.slice (6);
+        const embed = new MessageEmbed()
         .setDescription(mama)
-        .setColor('RANDOM')
-        .setThumbnail('https://i.imgur.com/w0y9l7X.png')
-        .setFooter('Copyright LWKD 2020', 'https://i.imgur.com/w0y9l7X.png');
+        .setColor(settings.svrclr)
+        //.setFooter(settings.copyright, 'https://i.imgur.com/w0y9l7X.png');
         message.channel.send(embed);
     }
     if (command === 'slap') {
@@ -168,14 +175,14 @@ bot.on('message', async message =>
         const embed = new MessageEmbed()
         .setAuthor(message.member.user.tag, message.author.displayAvatarURL(), '')
         .setImage('https://media.giphy.com/media/reXcrlJ3OhvDq/giphy.gif')
-        .setFooter('Copyright LWKD 2020', 'https://i.imgur.com/w0y9l7X.png')
+        //.setFooter(settings.copyright, 'https://i.imgur.com/w0y9l7X.png')
         message.channel.send(embed);
     }
     if (command === 'kiss') {
         const embed = new MessageEmbed()
         .setAuthor(message.member.user.tag, message.author.displayAvatarURL(), '')
         .setImage('https://media.giphy.com/media/perRo4txxsFxe/giphy.gif')
-        .setFooter('Copyright LWKD 2020', 'https://i.imgur.com/w0y9l7X.png')
+        //.setFooter(settings.copyright, 'https://i.imgur.com/w0y9l7X.png')
         message.channel.send(embed);
     }
 
@@ -204,7 +211,7 @@ bot.on('message', async message =>
         .setTitle(settings.svr)
         .setColor('RANDOM')
         .setImage(user.displayAvatarURL())
-        .setFooter('Copyright LWKD 2020', 'https://i.imgur.com/w0y9l7X.png')
+        //.setFooter(settings.copyright, 'https://i.imgur.com/w0y9l7X.png')
         message.channel.send(embed);
     }
 });
